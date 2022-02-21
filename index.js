97a40b72ad00e011b10a395762dcbbef7569bea5
Scott Hill <scott.c.hill.jr@gmail.com>
2022-02-21 16:53:45 -0600

    commit tree d891d463e6ccf0cb8af07fd8a0f5e593e950486b

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +15,24 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+
+  iterate through each num in array
+  identify a complement that adds to target;
+  comp = target - num
+  iterate through rest of array
+  check if any num is compliment
+  if so, return true
+  if i reach the end of array, return false
 */
 
 /*
   Add written explanation of your solution here
+
+  make a function called hasTargetSum
+  2 numbers from the array must equal the target
+  Example [1,2,3,4] and target is 6 means this is true because 2+4=6
+  For each number, check for another to add to equal target
+
 */
 
 // You can run `node index.js` to view these console logs
